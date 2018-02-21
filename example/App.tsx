1bf9224d2177083d637a06b7e220bca23ee65c05
Vincent Timofti <vincent.timofti@gmail.com>
2018-02-21 16:17:44 +0100

    Init project

---
+++ example/App.tsx
@@ -0,0 +1,5 @@
+import * as ReactDOM from "react-dom";
+import * as React from "react";
+import Slider from "../src/components/Slider";
+
+ReactDOM.render(<Slider />, document.getElementById("root"));
